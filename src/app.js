9bd4bf9276a26903302fd057541493601bb16c02
Harsika Singh <117065981+harsikaSingh@users.noreply.github.com>
2023-04-26 20:31:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ const app = express();
 //Middlewares
 app.use(express.json());
 
-// Write a GET Request to get the next number from the input 'num'.
-// Endpoint : /api/get-next-num
-// Return the response as {message : , status: }
+//GET request to get the next number from the input 'num'
+app.get('/api/get-next-num', (req, res) => {
+    const num = req.body.num;
+    if (!num || isNaN(num)) {
+        res.status(400).json({ message: "Invalid number", status: "failure" });
+    }
+    else {
+        const nextNum = num + 1;
+        res.status(200).json({ message: nextNum, status: "success" });
+    }
+});
 
 module.exports = app;
